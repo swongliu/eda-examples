d3ece6c48a3ceb4ef77620ef0b26e6995cd9ec21
Conan <you@example.com>
2021-07-31 14:39:20 +1000

    updated code tab

---
+++ Untitled.R
@@ -0,0 +1,51 @@
+1+1
+
+2+2
+
+variable <- 2
+
+class(name)
+
+c <- "word"
+
+class(c)
+
+vector <- c(1, 2, 3, 4)
+
+vector2 <- c("one", "two", "three", "four")
+
+vector[2]
+
+vector[3]
+
+vector[4]
+
+vector1 <- c(1, 2, 3, 4)
+
+vector <- c("one", "one", "one")
+
+vector <- c('one', 'one')
+
+length(vector1)
+
+vector2 <- c(1, 2)
+
+length(vector2)
+
+#a caveat of R 
+cbind(vector1, vector2)
+
+mean(vector1)
+
+median(vector1)
+
+x <- c(1, 2, 3, 4)
+y <- c(2, 3, 4, 5)
+
+plot(x, y)
+
+hist(x)
+
+barplot(x)
+
+
